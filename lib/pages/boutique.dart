ed830f83ea36025d61476c5bbd47695cd6883510
Mounina Diaby <diabymounina@gmail.com>
2023-09-22 16:26:13 +0100

    first commit

---
+++ lib/pages/boutique.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class Produit {
+  final String nom;
+  final String description;
+  final double prix;
+  final String image;
+
+  Produit({
+    required this.nom,
+    required this.description,
+    required this.prix,
+    required this.image,
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BoutiquePage(),
+    );
+  }
+}
+
+class BoutiquePage extends StatefulWidget {
+  @override
+  _BoutiquePageState createState() => _BoutiquePageState();
+}
+
+class _BoutiquePageState extends State<BoutiquePage> {
+  List<Produit> produits = [
+    Produit(
+      nom: "Tomate",
+      description: "Description du produit 1",
+      prix: 1000,
+      image: "images/product-5.jpg",
+    ),
+    Produit(
+      nom: "Carotte",
+      description: "Description du produit 2",
+      prix:1500,
+      image: "images/product-7.jpg",
+    ),
+    
+     Produit(
+      nom: "Poivron",
+      description: "Description du produit 2",
+      prix:1500,
+      image: "images/product-1.jpg",
+    ),
+    Produit(
+      nom: "Piment",
+      description: "Description du produit 2",
+      prix:1500,
+      image: "images/product-12.jpg",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SAMANDUGU'), 
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Ajouter une fonctionnalité de recherche
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.filter_list), 
+            onPressed: () {
+              // Ajouter une fonctionnalité de filtrage
+            },
+          ),
+        ],
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2 / 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        padding: EdgeInsets.all(10),
+        itemCount: produits.length,
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Image.network(
+                    produits[index].image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        produits[index].nom,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        produits[index].description,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        '${produits[index].prix.toStringAsFixed(2)} FCFA',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+  
+}
+ 
---
+++ lib/pages/profil.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profil',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      // Ici, normalement, vous envoyez les données (_email et _password) à votre backend pour authentification
+      print('Email: $_email, Password: $_password');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Connexion')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer votre email';
+                  }
+                  // Vérification supplémentaire pour l'email peut être ajoutée ici
+                  return null;
+                },
+                onSaved: (value) {
+                  _email = value!;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Mot de passe'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer votre mot de passe';
+                  }
+                  // Vérifications supplémentaires pour le mot de passe peuvent être ajoutées ici
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value!;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text('Se connecter'),
+                onPressed: _submitForm,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
